401c34f8a05116fabd75431e3b0437e5e7bb111e
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2018-02-23 15:32:49 +0100

    Update mailserver enode address to the Whisper v6 one

--- src/status_im/constants.cljs
+++ src/status_im/constants.cljs
@@ -87,10 +87,10 @@
 (def default-wnodes
   {"main" {:id      "main"
            :name    "Status mailserver"
-           :address "enode://72d8d7d1bd17621d0becf99f51d85662336f40688757b63c4ac9794acab1913ffd81c151e19c7a63d1f9b8a8bba2f3ec630a760ce186c8ea55f9efd7389da4e1@163.172.177.138:40404"}
+           :address "enode://1b995ba494e880e9b55fa0c093eb94db2d9de65de473d0fbd173f1d5af86f1c99da57e68fb00e58303c2b7abbbcd59396c7293c6c865ead4e5334feab4397feb@51.15.194.39:30303"}
    "test" {:id      "test"
            :name    "Status mailserver (test)"
-           :address "enode://e2fdcf8a55d9d1ab75b492c3b9a99c206cf5ceede3db716033d0605205218c9d9a406d04b3460f4e8a64f16907ab78ba88e0e46324568bf58b3329e2c5483faa@163.172.177.138:30353"}})
+           :address "enode://1b995ba494e880e9b55fa0c093eb94db2d9de65de473d0fbd173f1d5af86f1c99da57e68fb00e58303c2b7abbbcd59396c7293c6c865ead4e5334feab4397feb@51.15.194.39:30303"}})
 
 ;; TODO(oskarth): Determine if this is the correct topic or not
 (def inbox-topic "0xaabb11ee")
