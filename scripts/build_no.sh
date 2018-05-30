cd4d1aca797a56e010e5c447b6832c7ca351888e
Igor Mandrigin <i@mandrigin.ru>
2018-05-30 14:51:00 +0200

    more tests

--- scripts/build_no.sh
+++ scripts/build_no.sh
@@ -18,32 +18,4 @@
 # * https://developer.apple.com/library/content/documentation/General/Reference/InfoPlistKeyReference/Articles/CoreFoundationKeys.html
 # 
 
-getNumber () {
-    echo "$BUILD" | sed 's/[^0-9]*//g'
-}
-
-REGEX='^build-[0-9]\+$' 
-
-# make sure we have all the tags
-git fetch --tags --quiet
-
-
-# even if the current commit has a tag already, it is normal that the same commit
-# is built multiple times (with different build configurations, for instance),
-# so we increment the build number every time.
-
-# find the last used build number
-BUILD=$(git tag -l --sort=-v:refname | grep -e "$REGEX" | head -n 1)
-# extract the number
-BUILD_NO=$(getNumber "$BUILD")
-# increment
-BUILD_NO="$((BUILD_NO+1))"
-
-if [ "$1" = "--tag" ]; then
-    echo "Tagging HEAD: build-$BUILD_NO" >&2
-    echo "You will need to 'git push --tags' to make this tag take effect." >&2
-    git tag "build-$BUILD_NO" HEAD
-fi
-
-# finally print build number
-echo "$BUILD_NO"
+echo "$BUILD_NUMBER"
