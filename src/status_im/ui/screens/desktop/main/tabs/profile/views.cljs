7fd0bd89e398707cdf43253fb3aa9d46261521f5
Vitaliy Vlasov <siphiuel@gmail.com>
2018-06-12 14:05:22 +0300

    Add share-contact-code

--- src/status_im/ui/screens/desktop/main/tabs/profile/views.cljs
+++ src/status_im/ui/screens/desktop/main/tabs/profile/views.cljs
@@ -2,6 +2,10 @@
   (:require-macros [status-im.utils.views :as views])
   (:require [re-frame.core :as re-frame]
             [status-im.ui.components.react :as react]
+            [status-im.ui.components.icons.vector-icons :as vector-icons]
+            [status-im.ui.components.colors :as colors]
+            [status-im.ui.components.qr-code-viewer.views :as qr-code-viewer]
+            [status-im.ui.screens.profile.user.styles :as styles]
             [status-im.ui.screens.profile.user.views :as profile]))
 
 (defn profile-badge [{:keys [name]}]
@@ -21,6 +25,27 @@
                  :ellipsizeMode   :middle}
      value]]]])
 
+(defn- show-qr [contact source value]
+  [react/modal "Some text"]
+  #_(re-frame/dispatch [:navigate-to :profile-qr-viewer {:contact contact
+                                                        :source  source
+                                                        :value   value}]))
+
+(defn share-contact-code [{:keys [public-key] :as current-account}]
+  [react/view
+   [react/touchable-highlight {:on-press #(show-qr current-account :public-key public-key) 
+                               :style {:background-color "#4360df" :opacity 0.1}}
+    [react/view (merge styles/share-contact-code {:opacity 1.0})
+     [react/view styles/share-contact-code-text-container
+      [react/text {:style       styles/share-contact-code-text
+                   :uppercase? true}
+       "Share my contact code"]]
+     [react/view {:style               styles/share-contact-icon-container
+                  :accessibility-label :share-my-contact-code-button}
+      [vector-icons/icon :icons/qr {:color colors/blue}]]]]
+   [qr-code-viewer/qr-code-viewer {:style styles/qr-code}
+    public-key "Share this code to start chatting" public-key]])
+
 (defn my-profile-info [{:keys [public-key]}]
   [react/view
    [profile-info-item
@@ -34,7 +59,8 @@
       [profile-badge current-account]]
      [react/view {:style {:height 1 :background-color "#e8ebec" :margin-horizontal 16}}]
      [react/view
-      [my-profile-info current-account]]
+      [share-contact-code current-account]
+      #_[my-profile-info current-account]]
      [react/view {:style {:height 1 :background-color "#e8ebec" :margin-horizontal 16}}]
      [react/touchable-highlight {:on-press #(re-frame/dispatch [:logout])
                                  :style {:margin-top 60}}
