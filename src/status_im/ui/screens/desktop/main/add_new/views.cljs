80152a39e3c80c51a91367f953fe8a4303822d00
Vitaliy Vlasov <siphiuel@gmail.com>
2018-06-13 18:38:39 +0300

    Some styling changes

--- src/status_im/ui/screens/desktop/main/add_new/views.cljs
+++ src/status_im/ui/screens/desktop/main/add_new/views.cljs
@@ -12,15 +12,16 @@
                   error-message [:new-contact-error-message]
                   topic [:get :public-group-topic]
                   account [:get-current-account]]
-    [react/view {:style {:flex 1 :background-color :white}}
+    [react/view {:style {:flex 1 :background-color :white :margin-left 24 :margin-right 37}}
      ^{:key "newcontact"}
-     [react/view {:style {:height 64 :align-items :flex-start :padding-horizontal 11 :justify-content :center}}
+     [react/view {:style {:height 64 :align-items :flex-start :justify-content :center}}
       [react/text {:style {:font-size 20 :color :black :font-weight "600"}}
        "New Chat"]]
-     [react/view {:style {:height 1 :background-color "#e8ebec" :margin-horizontal 16}}]
-     [react/view {:style {:height     90 :margin-horizontal 16 :margin-bottom 16 :background-color :white :border-radius 12}}
+     [react/text {:style {:font-size 14}} "Contact code"]
+     [react/view {:style {:height 1 :background-color "#e8ebec"}}]
+     [react/view {:style {:height 90 :margin-bottom 16 :background-color :white :border-radius 12}}
                           ;:box-shadow "0 0.5px 4.5px 0 rgba(0, 0, 0, 0.04)"}}
-      [react/view {:style {:flex-direction :row :margin-horizontal 16 :margin-top 16}}
+      [react/view {:style {:flex-direction :row :margin-top 16}}
        [react/text-input {:placeholder "0x..."
                           :flex 1
                           :on-change   (fn [e]
@@ -31,14 +32,30 @@
         [react/view {:style {:width 140 :height 45 :border-radius 8 :background-color (if error-message "#eef2f5" "#4360df") :align-items :center
                              :justify-content :center}}
          [react/text {:style {:font-size 16 :color (if error-message "#939ba1" :white)}} "Start chat"]]]]]
+     ^{:key "choosecontact"}
+     [react/view
+      (when (seq contacts) [react/text {:style {:font-size 14}} "Or choose a contact"])
+      [react/view {:style {:margin-top 12}} 
+       (doall
+        (for [c contacts]
+        ^{:key (:whisper-identity c)}
+        [react/touchable-highlight {:on-press #(do
+                                                 (re-frame/dispatch [:set :contacts/new-identity (:whisper-identity c)])
+                                                 (re-frame/dispatch [:add-contact-handler (:whisper-identity c)]))}
+         [react/view {:style {:flex-direction "row"
+                             :align-items :center
+                             :margin-bottom 16}}
+         [react/image {:style {:width 46 :height 46 :border-radius 46 :margin-right 16}
+                       :source {:uri (:photo-path c)}}]
+         [react/text {:style {:font-size 14}} (:name c)]]]))]]
      ^{:key "publicchat"}
-     [react/view {:style {:height 64 :align-items :flex-start :padding-horizontal 11 :justify-content :center}}
+     [react/view {:style {:height 64 :align-items :flex-start :justify-content :center}}
       [react/text {:style {:font-size 20 :color :black :font-weight "600"}}
        "Join Public Chat"]]
-     [react/view {:style {:height 1 :background-color "#e8ebec" :margin-horizontal 16}}]
-     [react/view {:style {:height     90 :margin-horizontal 16 :margin-bottom 16 :background-color :white :border-radius 12}}
+     [react/view {:style {:height 1 :background-color "#e8ebec"}}]
+     [react/view {:style {:height 90 :margin-bottom 16 :background-color :white :border-radius 12}}
                           ;:box-shadow "0 0.5px 4.5px 0 rgba(0, 0, 0, 0.04)"}}
-      [react/view {:style {:flex-direction :row :margin-horizontal 16 :margin-top 16}}
+      [react/view {:style {:flex-direction :row :margin-top 16}}
        [react/view {:style {:flex 1}}
         [react/text-input {:placeholder "#"
                            :flex 1
